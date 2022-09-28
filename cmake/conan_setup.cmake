40e9f8e0b67dacac9d65c36ddd6b50ddbd92e44e
Robert Steffens <39485357+ki-bo@users.noreply.github.com>
2022-09-28 13:11:14 +0200

    Support mingw32 Linux-to-Windows cross compiling

--- cmake/conan_setup.cmake
+++ cmake/conan_setup.cmake
@@ -8,15 +8,65 @@ if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
 endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
+
 conan_cmake_configure(REQUIRES 
                         libusb/1.0.26
                         libpng/1.6.37
+                      OPTIONS
+                        libusb:shared=False
+                        libpng:shared=False
                       GENERATORS cmake_find_package)
+
+
 conan_cmake_autodetect(settings ARCH x86_64)
 if (APPLE)
   list(APPEND settings "os.version=${CMAKE_OSX_DEPLOYMENT_TARGET}")
 endif()
-conan_cmake_install(PATH_OR_REFERENCE .
-                    BUILD missing
-                    REMOTE conancenter
-                    SETTINGS ${settings})
+
+if (CMAKE_C_COMPILER_ID STREQUAL "GNU")
+  set(conan_compiler "gcc")
+elseif (CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+  set(conan_compiler "msvc")
+elseif (CMAKE_C_COMPILER_ID STREQUAL "Clang")
+  set(conan_compiler "clang")
+elseif (CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
+  set(conan_compiler "apple-clang")
+endif()
+
+string(REGEX MATCH "^([0-9]+)\\.?.*$" COMPILER_VERSION_MAJOR ${CMAKE_C_COMPILER_VERSION})
+set(COMPILER_VERSION_MAJOR ${CMAKE_MATCH_1})
+
+if (CMAKE_CROSSCOMPILING)
+  conan_cmake_install(PATH_OR_REFERENCE .
+                      BUILD missing
+                      REMOTE conancenter
+                      SETTINGS_BUILD
+                        ${settings}
+                      SETTINGS_HOST
+                        os=${CMAKE_SYSTEM_NAME}
+                        compiler=${conan_compiler}
+                        compiler.version=${COMPILER_VERSION_MAJOR}
+                      ENV_HOST
+                        CONAN_CMAKE_FIND_ROOT_PATH=${CMAKE_FIND_ROOT_PATH}
+                        CHOST=${TOOLCHAIN_PREFIX}
+                        CC=${CMAKE_C_COMPILER}
+                        CXX=${CMAKE_CXX_COMPILER}
+                        RC=${CMAKE_RC_COMPILER}
+                      )
+else()
+  conan_cmake_install(PATH_OR_REFERENCE .
+                      BUILD missing
+                      REMOTE conancenter
+                      SETTINGS
+                        ${settings}
+                        os=${CMAKE_SYSTEM_NAME}
+                        compiler=${conan_compiler}
+                        compiler.version=${COMPILER_VERSION_MAJOR}
+                      ENV
+                        CONAN_CMAKE_FIND_ROOT_PATH=${CMAKE_FIND_ROOT_PATH}
+                        CHOST=${TOOLCHAIN_PREFIX}
+                        CC=${CMAKE_C_COMPILER}
+                        CXX=${CMAKE_CXX_COMPILER}
+                        RC=${CMAKE_RC_COMPILER}
+                      )
+endif()
